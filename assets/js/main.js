c35c710c284e706ee9093aca4cc7efa9235539ed
Alfredo <alfredogregorio.lozano@gmail.com>
2021-01-08 15:17:54 +0100

    creazione repo

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+let app = new Vue({
+    el: '#app',
+    data:{
+
+    },
+    methods:{
+
+    },
+    mounted(){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(function(response){
+            console.log(response);
+        })
+    }
+})
